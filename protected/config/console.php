c53d7dc3914122082e178bca3b706aa739d5163c
Андрей <aasvetlichniy@gmail.com>
2015-02-17 19:14:33 +0400

    игнор

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,18 +1,21 @@
 <?php
+
 // This is the configuration for yiic console application.
 // Any writable CConsoleApplication properties can be configured here.
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => 'My Console Application',
+
     // preloading 'log' component
     'preload' => array('log'),
+
     // application components
     'components' => array(
         'db' => array(
             'connectionString' => 'mysql:host=localhost;dbname=dipstart',
             'emulatePrepare' => true,
-            'username' => 'dipstart',
-            'password' => '',
+            'username' => 'root',
+            'password' => 'root',
             'charset' => 'utf8',
         ),
         // uncomment the following to use a MySQL database
