75ac3ed0937fd87f1bada57e7e7ce21309654e60
coolfire79 <coolfire@inbox.ru>
2015-02-23 15:44:56 +0300

    Change site main page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,5 +6,5 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Добро пожаловать, <b><?php echo Yii::t('site',$role); ?></b>, на наш сайт <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Мы рады приветствовать Вас на нашем сайте. Для начала работы <a href="<?php echo $this->createUrl('/user/login');?>">войдите</a> либо <a href="<?php echo $this->createUrl('/user/registration');?>">зарегистрируйтесь</a>.</p>
+<p>Мы рады приветствовать Вас на нашем сайте. Для начала работы <a href="<?php echo $this->createUrl('/user/login');?>">войдите</a> либо <a href="<?php echo $this->createUrl('/user/registration&role=Customer');?>">зарегистрируйтесь</a>.</p>
 
