09c40210d1e5adc7762f461392a37138e5597d6c
coolfire79 <coolfire@inbox.ru>
2015-02-28 10:59:00 +0300

    Fix error 403

--- protected/modules/project/controllers/ZakazController.php
+++ protected/modules/project/controllers/ZakazController.php
@@ -32,7 +32,7 @@ class ZakazController extends Controller
 					'users'=>array('*'),
 				),*/
                 array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                    'actions'=>array('index','create','update', 'admin', 'preview', 'moderationAnswer', 'yiifilemanagerfilepicker','list', 'ownList','customerOrderList', 'uploadPayment'),
+                    'actions'=>array('view','index','create','update', 'admin', 'preview', 'moderationAnswer', 'yiifilemanagerfilepicker','list', 'ownList','customerOrderList', 'uploadPayment'),
                     'users'=>array('@'),
                 ),
                 array('allow', // allow admin user to perform 'admin' and 'delete' actions
