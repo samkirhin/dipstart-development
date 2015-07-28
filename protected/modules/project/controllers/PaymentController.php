d6b3c1a838c7d76dee65e795c90f50ac2a8006b1
Konstantin Tsukur <nicdnepr@gmail.com>
2015-07-28 22:10:53 +0300

    fix
    проверка оплаты автору

--- protected/modules/project/controllers/PaymentController.php
+++ protected/modules/project/controllers/PaymentController.php
@@ -139,7 +139,7 @@ class PaymentController extends Controller {
         $payment->work_price = $this->_request->getParam('work_price');
         $paying              = (int) $this->_request->getParam('to_pay');
         
-        if ( ($payment->work_price > 0) && ($payment->to_pay + $paying > $payment->work_price) ) {
+        if ($payment->to_pay + $paying > $payment->payed) {
             echo CJson::encode(['Оплата превышает лимит']);
             Yii::app()->end();
         }
