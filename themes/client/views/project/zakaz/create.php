9b7c2027359e697bc489059191df54079bb8689d
akoch-ov <ako40ff@gmail.com>
2015-09-27 04:40:47 +0400

    new-order-form

--- themes/client/views/project/zakaz/create.php
+++ themes/client/views/project/zakaz/create.php
@@ -1,35 +1,159 @@
+<?php Yii::app()->getClientScript()->registerCssFile(Yii::app()->theme->baseUrl.'/css/reset.css');?>
 <?php Yii::app()->getClientScript()->registerCssFile(Yii::app()->theme->baseUrl.'/css/custom.css');?>
+<?php Yii::app()->getClientScript()->registerCssFile(Yii::app()->theme->baseUrl.'/css/plate-form.css');?>
+<?php Yii::app()->getClientScript()->registerCssFile(Yii::app()->theme->baseUrl.'/css/plate-form-media.css');?>
 <?php
 /* @var $this ZakazController */
 /* @var $model Zakaz */
 /* @var $form CActiveForm */
-
-$this->breadcrumbs=array(
-	ProjectModule::t('Zakazs')=>array('index'),
-	ProjectModule::t('Create'),
-);
-
 ?>
-<div class="row">
-    <div class="col-xs-12" style="margin-bottom: 20px;">
-<h1><?=ProjectModule::t('Create Zakaz')?></h1>
-        <?php
-        ?>
+    <div class="container form-container">
+		<?php $form = $this->beginWidget('CActiveForm', array(
+			'id'=>'zakaz-form',
+			'action'=>isset ($model->id) ? $this->createUrl('zakaz/update', ['id'=>$model->id]) : '',
+			//'type' => 'horizontal',
+			//'htmlOptions' => array('class' => 'well'),
+			// Please note: When you enable ajax validation, make sure the corresponding
+			// controller action is handling ajax validation correctly.
+			// There is a call to performAjaxValidation() commented in generated controller code.
+			// See class documentation of CActiveForm for details on this.
+			'enableAjaxValidation'=>false,
+
+		)); ?>
+		
+			<p class="note"><?=ProjectModule::t('Fields with <span class="required">*</span> are required.')?></p>
+			
+            <?php echo $form->errorSummary($model); ?>
+			<div class="form-items">
+			
+			<?php
+			if(Campaign::getId()){ ?>
+				<div class="form-item">
+                <?php echo $form->labelEx($model,'max_exec_date');?>
+                <?php
+                $this->widget('ext.juidatetimepicker.EJuiDateTimePicker', array(
+                    'model' => $model,
+                    'attribute' => 'dbmax_exec_date',
+                ));?>
+				</div><?php
+				$projectFields = $model->getFields();
+				if ($projectFields) {
+					foreach($projectFields as $field) {
+						if ($field->field_type=="LIST"){
+							echo '<div class="form-item">';
+							echo $form->labelEx($model,$field->varname).'<br/>';
+							$models = Catalog::model()->findAllByAttributes(array('field_varname'=>$field->varname));
+							$list = CHtml::listData($models, 'id', 'cat_name');
+							echo $form->dropDownList($model, $field->varname, $list, array('empty' => ProjectModule::t('Select a category'),'class'=>'form-control'));
+							echo $form->error($model,$field->varname);
+							echo '</div>';
+						} elseif ($field->field_type=="TIMESTAMP" || $field->field_type=="DATE") {
+							?>
+							<div class="form-item" style="position: relative; float: left; width: 100%;">
+								<label style="position: relative; float: left; width: 100px;"><?php echo $form->labelEx($model,$field->varname);?></label>
+								<?php
+								$this->widget('ext.juidatetimepicker.EJuiDateTimePicker', array(
+									'model' => $model,
+									'attribute' => $field->varname,
+								));?>
+							</div><?php
+						} elseif ($field->field_type=="TEXT") {
+							echo '<div class="form-item">';
+							echo $form->labelEx($model,$field->varname).'<br/>';
+							echo$form->textArea($model,$field->varname,array('rows'=>6, 'cols'=>50, 'class'=>'form-control'));
+							echo '</div>';
+						} else {
+							echo '<div class="form-item">';
+							echo $form->labelEx($model,$field->varname).'<br/>';
+							echo $form->textField($model,$field->varname,array('size'=>60,'maxlength'=>(($field->field_size)?$field->field_size:255), 'class'=>'form-control'));
+							echo '</div>';
+						}
+					}
+				}
+			} ?>
+		
+            <!--<div class="form-items">
+                <div class="form-item">
+                    <h3 class="item-name">Дата и время исполнения заказа</h3>
+                    <input type="text">
+                </div>
+
+                <div class="form-item">
+                    <h3 class="item-name">Название работы</h3>
+                    <input type="text">
+                </div>
+
+                <div class="form-item" style="">
+                    <h3 class="item-name">Специализация проекта</h3>
+                    <select>
+                        <option value="">Выберите категорию</option>
+                        <option value="5">Все</option>
+                        <option value="6">Дизайн сайта </option>
+                        <option value="7">Верстка сайта </option>
+                        <option value="8">Копирайтинг</option>
+                        <option value="9">Рерайтинг</option>
+                        <option value="10">Оптимизация (СЕО)</option>
+                        <option value="11">Видео</option>
+                        <option value="12">Программирование</option>
+                        <option value="13">Постинг</option>
+                        <option value="14">Презентации</option>
+                        <option value="15">Диктор</option>
+                        <option value="16">Перевод с Рус на Eng</option>
+                        <option value="17">Другое</option>
+                        <option value="18">Бизнес под ключ</option>
+                        <option value="19">Проект под ключ</option>
+                        <option value="20">Сайт под ключ</option>
+                        <option value="21">Лендинг под ключ</option>
+                        <option value="23">Тестирование</option>
+                    </select>
+                </div>
+
+                <div class="form-item">
+                    <h3 class="item-name">План</h3>
+                    <textarea rows="4"></textarea>
+                </div>
+
+                <div class="form-item">
+                    <h3 class="item-name">Описание</h3>
+                    <textarea rows="4"></textarea>
+                </div>
+
+                <div class="form-item">
+                    <h3 class="item-name">Дополнительные требования или рекомендации</h3>
+                    <textarea rows="4"></textarea>
+                </div>
+
+                <div class="form-item">
+                    <h3 class="item-name">Add an Abstract page to my paper</h3>
+                    <input type="text">
+                </div>
+				-->
+            </div>
+            <!--<input type="submit" class="create-order-button">-->
+			<?php echo CHtml::submitButton('Загрузить', array('class' => 'create-order-button') ); ?>
+		
+		<?php $this->endWidget(); ?>
+        <!--</form>-->
+    
+		
+
+		
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <!--<div class="col-xs-12" style="margin-bottom: 20px;">
+    
         <div class="form" style="margin-bottom: 30px;">
 
-            <?php $form = $this->beginWidget('CActiveForm', array(
-                'id'=>'zakaz-form',
-                'action'=>isset ($model->id) ? $this->createUrl('zakaz/update', ['id'=>$model->id]) : '',
-                //'type' => 'horizontal',
-                //'htmlOptions' => array('class' => 'well'),
-                // Please note: When you enable ajax validation, make sure the corresponding
-                // controller action is handling ajax validation correctly.
-                // There is a call to performAjaxValidation() commented in generated controller code.
-                // See class documentation of CActiveForm for details on this.
-                'enableAjaxValidation'=>false,
-            )); ?>
 
-            <p class="note"><?=ProjectModule::t('Fields with <span class="required">*</span> are required.')?></p>
+
+            
 
             <?php echo $form->errorSummary($model);
 			// campaign! -------------
@@ -153,7 +277,7 @@ $this->breadcrumbs=array(
                 <?php echo CHtml::submitButton($model->isNewRecord ? ProjectModule::t('Create') : ProjectModule::t('Save'), array ('class'=>'btn btn-primary btn-save')); ?>
             </div>
 
-            <?php $this->endWidget();
+            <?php //$this->endWidget();
             ?>
 
         </div><!-- form -->
@@ -180,14 +304,16 @@ $this->breadcrumbs=array(
                     <?php echo $form->fileField($upload,'file'); ?>
                 </div>
 
-                <div class="row buttons">
-                    <?php echo CHtml::submitButton('Загрузить'); ?>
-                </div>
-
-                <?php $this->endWidget(); ?>
+<!-- End form --><?php $this->endWidget(); ?>
 
             </div>
 
         <?php endif; ?>
     </div>
 </div>
+
+
+<?php
+    Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl.'/js/masonry.min.js', CClientScript::POS_END);
+    Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl.'/js/common-masonry.js', CClientScript::POS_END);
+?>
