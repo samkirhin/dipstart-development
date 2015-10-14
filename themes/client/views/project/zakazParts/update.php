e68fde9635d4971adea6e47873e29101ddaa782e
oldbadger <gregory.pelipenko@gmail.com>
2015-10-14 08:59:45 +0300

    #113 небольшое дополнение
    Signed-off-by: oldbadger <gregory.pelipenko@gmail.com>

--- themes/client/views/project/zakazParts/update.php
+++ themes/client/views/project/zakazParts/update.php
@@ -16,6 +16,6 @@ $this->menu=array(
 );
 ?>
 
-<h1><?= ProjectModule::t('Update ZakazParts').' '.$model->id; ?></h1>
+<h1><?= ProjectModule::t('Update ZakazParts').' #'.$model->id; ?></h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
