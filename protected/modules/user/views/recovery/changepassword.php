c3422e85c43588ba58e5188bec9de6267eca7680
oldbadger <gregory.pelipenko@gmail.com>
2015-10-30 12:35:59 +0200

    #168
    Signed-off-by: oldbadger <gregory.pelipenko@gmail.com>

--- protected/modules/user/views/recovery/changepassword.php
+++ protected/modules/user/views/recovery/changepassword.php
@@ -4,7 +4,8 @@ $this->breadcrumbs=array(
 	UserModule::t("Change Password"),
 );
 ?>
-
+<?php Yii::app()->getClientScript()->registerCssFile(Yii::app()->theme->baseUrl.'/css/skin2.css');?>
+<div class="col-xs-offset-3 col-xs-6 login-form-bg">
 <h1><?php echo UserModule::t("Change Password"); ?></h1>
 
 
@@ -14,23 +15,24 @@ $this->breadcrumbs=array(
 	<p class="note"><?php echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></p>
 	<?php echo CHtml::errorSummary($form); ?>
 	
-	<div class="row">
+	<div class="row" style="align: center;">
 	<?php echo CHtml::activeLabelEx($form,'password'); ?>
-	<?php echo CHtml::activePasswordField($form,'password'); ?>
+	<?php echo CHtml::activePasswordField($form,'password',array('style'=>'width: 100%')); ?>
 	<p class="hint">
 	<?php echo UserModule::t("Minimal password length 4 symbols."); ?>
 	</p>
 	</div>
 	
-	<div class="row">
+	<div class="row" style="align: center;">
 	<?php echo CHtml::activeLabelEx($form,'verifyPassword'); ?>
-	<?php echo CHtml::activePasswordField($form,'verifyPassword'); ?>
+	<?php echo CHtml::activePasswordField($form,'verifyPassword',array('style'=>'width: 100%')); ?>
 	</div>
-	
-	
+		
+	<br>
 	<div class="row submit">
 	<?php echo CHtml::submitButton(UserModule::t("Save")); ?>
 	</div>
 
 <?php echo CHtml::endForm(); ?>
 </div><!-- form -->
+</div><!-- <div class="col-xs-offset-3 col-xs-6 login-form-bg"> -->
