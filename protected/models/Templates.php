08d5510601bda867ba495fc53ca8fef2f60d04fb
akoch-ov <ako40ff@gmail.com>
2016-04-26 19:56:30 +0300

    templates variables fix

--- protected/models/Templates.php
+++ protected/models/Templates.php
@@ -152,4 +152,54 @@ class Templates extends CActiveRecord
 			$custom_arr[$item->name] = $item->text;
 		return $custom_arr;
 	}
+	
+    protected function replaceBBCode($text_post) {
+        $str_search = array(
+            "#\\\n#is",
+            "#\[b\](.+?)\[\/b\]#is",
+            "#\[i\](.+?)\[\/i\]#is",
+            "#\[u\](.+?)\[\/u\]#is",
+            "#\[code\](.+?)\[\/code\]#is",
+            "#\[quote\](.+?)\[\/quote\]#is",
+            "#\[url=(.+?)\](.+?)\[\/url\]#is",
+            "#\[url\](.+?)\[\/url\]#is",
+            "#\[img\](.+?)\[\/img\]#is",
+            "#\[size=(.+?)\](.+?)\[\/size\]#is",
+            "#\[color=(.+?)\](.+?)\[\/color\]#is",
+            "#\[list\](.+?)\[\/list\]#is",
+            "#\[listn](.+?)\[\/listn\]#is",
+            "#\[\*\](.+?)\[\/\*\]#"
+        );
+        $str_replace = array(
+            "<br />",
+            "<b>\\1</b>",
+            "<i>\\1</i>",
+            "<span style='text-decoration:underline'>\\1</span>",
+            "<code class='code'>\\1</code>",
+            "<table width = '95%'><tr><td>Цитата</td></tr><tr><td class='quote'>\\1</td></tr></table>",
+            "<a href='\\1'>\\2</a>",
+            "<a href='\\1'>\\1</a>",
+            "<img src='\\1' alt = 'Изображение' />",
+            "<span style='font-size:\\1%'>\\2</span>",
+            "<span style='color:\\1'>\\2</span>",
+            "<ul>\\1</ul>",
+            "<ol>\\1</ol>",
+            "<li>\\1</li>"
+        );
+        return preg_replace($str_search, $str_replace, $text_post);
+    }
+
+    public function insertVariables($in){
+        $in = $this->replaceBBCode($in);
+        preg_match_all("/\{(.+?)\}/is",$in,$out);
+        foreach ($out[1] as $k=>$o) {
+            $var = explode('_', $o);
+            $model_name = ucfirst(array_shift($var));
+            $project_model = $model_name::model()->findByPk(Yii::app()->session['project_id']);
+            $field = $project_model;
+            foreach ($var as $v) $field = $field->$v;
+            $fields[$k] = $field;
+        }
+        return str_replace($out[0],$fields,$in);
+	}
 }
