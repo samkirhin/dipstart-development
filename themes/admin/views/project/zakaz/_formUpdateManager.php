5cdb5868cc6761be9ed9f6eb8a157e931fe95f99
akoch-ov <ako40ff@gmail.com>
2016-04-28 21:20:45 +0300

    small fixes

--- themes/admin/views/project/zakaz/_formUpdateManager.php
+++ themes/admin/views/project/zakaz/_formUpdateManager.php
@@ -138,7 +138,7 @@
 </div>
 
 <?php
-$this->widget('application.modules.project.widgets.tips.TipsWidget', array(
-    'project' => $model
-));
+//$this->widget('application.modules.project.widgets.tips.TipsWidget', array(
+//    'project' => $model
+//));
 ?>
