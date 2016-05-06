777f9feeed8a4bf61a3defc4200f41d534aa5ef5
b4djo <robertooo1990@gmail.com>
2016-05-06 09:09:55 +0300

    fix cron events

--- protected/commands/EventsCommand.php
+++ protected/commands/EventsCommand.php
@@ -12,23 +12,21 @@ class EventsCommand extends CConsoleCommand {
 		if (is_array($usersModel))
 			foreach ($usersModel as $user) {
 				
-				$profileModel = Profile::model()->findByPk($user->id);
+				$profileModel = User::model()->with(array('profile'=>array('select'=>false, 'joinType'=>'INNER JOIN', 'condition'=>'profile.notification="1"')))->findByPk($user->id);
 				if ($profileModel===null) throw new CHttpException(404, 'Данные профиля пользователя не найдены.');
 				
-				if ($profileModel->notification == '1') {
-					foreach ($user->zakaz as $zakaz) {
-						$time = explode(';', $profileModel->notification_time); // время X, за которое надо уведомлять (количество часов и минут), формат "5;48"
-						$date = date('Y-m-d H:i',strtotime($zakaz->author_informed));
-						$date = strtotime($date)-(int)$time[0]*60*60-(int)$time[1]*60;
-						if (strtotime(date('Y-m-d H:i',time())) == $date) {
-							$templatesModel = Templates::model()->findByPk(21);
-							
-							$email = new Emails;
-							$email->from_id	= 1;
-							$email->to_id 	= $user->id;
-							$email->name 	= $user->full_name;
-							$email->sendTo($user->email, $templatesModel->title, $templatesModel->text);
-						}
+				foreach ($user->zakaz as $zakaz) {
+					$time = explode(';', $profileModel->notification_time); // время X, за которое надо уведомлять (количество часов и минут), формат "5;48"
+					$date = date('Y-m-d H:i',strtotime($zakaz->author_informed));
+					$date = strtotime($date)-(int)$time[0]*60*60-(int)$time[1]*60;
+					if (strtotime(date('Y-m-d H:i',time())) == $date) {
+						$templatesModel = Templates::model()->findByPk(21);
+						
+						$email = new Emails;
+						$email->from_id	= 1;
+						$email->to_id 	= $user->id;
+						$email->name 	= $user->full_name;
+						$email->sendTo($user->email, $templatesModel->title, $templatesModel->text);
 					}
 				}
 			}
