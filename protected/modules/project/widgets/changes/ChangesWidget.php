aa2a091329f9a233a94b453a1ac10362205b5d2d
akoch-ov <ako40ff@gmail.com>
2016-05-27 22:44:20 +0300

    changes for author fix

--- protected/modules/project/widgets/changes/ChangesWidget.php
+++ protected/modules/project/widgets/changes/ChangesWidget.php
@@ -23,19 +23,20 @@ class ChangesWidget extends CWidget
     protected $userObj;
 
     public function init() {
+		//echo 'project_id =' . (int)$this->project->id .' '. ((User::model()->isManager() || User::model()->isAdmin()) ? '' : (' AND (user_id = '.((Yii::app()->user->id)?Yii::app()->user->id:(-1)).' OR moderate=1)' )).'!!!';
         $this->userObj = User::model();
         $this->changes = new CArrayDataProvider(Yii::app()->db->createCommand()
             ->select('CONCAT("/' . ProjectChanges::$file_path . '/",file)  as `file`, file as `filename`, comment, id, moderate, date_create')
             ->from(ProjectChanges::model()->tableName())
-            ->where('project_id =' . (int)$this->project->id . (User::model()->isManager() || User::model()->isAdmin() ? '' : (' AND (user_id = '.(Yii::app()->user->id)?Yii::app()->user->id:(-1).' OR moderate=1)' )))
+            ->where('project_id =' . (int)$this->project->id . (User::model()->isManager() || User::model()->isAdmin() ? '' : (' AND (user_id = '.((Yii::app()->user->id)?Yii::app()->user->id:(-1)).' OR moderate=1)' )))
             ->queryAll(),
             array(
                 'pagination' => false,
             ));
     }
 
-    public function run()
-    {
+    public function run() {
+		//echo 'go'.count($this->changes->rawData);
         if (count($this->changes->rawData) > 0 || User::model()->isCustomer() || $this->isCorrector || User::model()->isManager() || User::model()->isAdmin()) {
             if(Yii::app()->user->isGuest || (User::model()->isAuthor() && !User::model()->isExecutor($this->project->id) && !$this->isCorrector)){
 				$this->render('simple', array('changes' => $this->changes));
